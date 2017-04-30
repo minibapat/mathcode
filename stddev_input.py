50b365e3bd4a5b3edf100f47e32f8820df2927e3
minibapat <mini.bapat@gmail.com>
2017-04-29 22:43:06 -0700

    Stddev with user input
    Standard deviation with user input

---
+++ stddev_input.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+import math
+
+def main():
+
+  # Read input from user, till he enters -1
+  list1 = read_list()
+  n = len(list1)
+  mysum = mylist_sum(list1)
+  mean1 = calculate_mean(mysum, n)
+  findsummation = calculate_summation(mean1, list1, n)
+  variance1 = calc_variance(findsummation, n)
+  mystandarddev = findsd(variance1)
+  print "The standard deviation of list:"
+  print list1
+  print " is " + str(mystandarddev)
+
+def read_list():
+  out_list=[]
+  while True:
+    a = int(raw_input("Enter number (-1 to end) : "))
+    if a==-1:
+      break
+    out_list.append(a)
+  return out_list
+
+
+def mylist_sum(mylist):
+  mysum1 = 0
+  for ii in mylist:
+    mysum1 = mysum1 + ii
+  return mysum1
+
+def calculate_mean(listsum, nums):
+  finalmean = listsum / (nums *1.0)
+  return finalmean
+
+def calculate_summation(mean1, list1, n):
+  mysum=0
+  for ii in list1:
+    diff1 = ii - mean1
+    finalseries = diff1*diff1
+    mysum = mysum + finalseries
+  return mysum
+
+def calc_variance(findsummation, n):
+  myvariance = findsummation/(n*1.0)
+  return myvariance
+
+def findsd(variance1):
+  finalstanddev = math.sqrt(variance1)
+  return finalstanddev
+
+
+main()
