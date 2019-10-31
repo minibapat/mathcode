c0d4735234ccb5f6f46e2f389910d20b74e533b2
minibapat <mini.bapat@gmail.com>
2019-10-30 23:23:08 -0700

    Finding the max of two numbers
    This program takes command line arguments, converts them to integers, and then passes them to a function that finds the max of the two numbers.

---
+++ findmax.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int findMax(int one, int two);
+
+int main(int argc, char *argv[]){
+
+   if (argc!=3) {
+    cerr << "Usage: " << argv[0] << " first number  second number"<< endl;
+    exit(1);
+   }
+
+
+   int first = stoi(argv[1]);
+   int second = stoi(argv[2]);
+
+   if(first == second){
+     cout<<"Both numbers are the same, so there is no max."<<endl;
+     return 0;
+   }
+
+   int max = findMax(first, second);
+   cout<<"The max of the two numbers is " << max<<endl;
+   return 0;
+
+}
+
+
+int findMax(int one, int two){
+   if(one>two)
+      return one;
+   return two;
+
+}
